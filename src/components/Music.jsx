0dcd221d8446c495813fc1b4229a4f8938737bf8
Samantha1919 <samantha.schif@gmail.com>
2026-02-25 17:30:53 +0100

    fix: grab and move images in Playground

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,9 +1,17 @@
-import React from "react";
 import { ArrowLeft } from "./icons";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { useRef } from "react";
-import { kidCudi,lorenzo,smiths,billy,tvGirl,sheAndHim,sunsetz,booba } from "../assets";
+import {
+  kidCudi,
+  lorenzo,
+  smiths,
+  billy,
+  tvGirl,
+  sheAndHim,
+  sunsetz,
+  booba,
+} from "../assets";
 
 function Music() {
   const navigate = useNavigate();
@@ -12,111 +20,116 @@ function Music() {
     {
       title: "Willing To Trust",
       artist: "Kid Cudi",
-      albumCover: kidCudi, 
+      albumCover: kidCudi,
       left: "5%",
       top: "2%",
     },
     {
       title: "Back to the old house",
       artist: "The Smiths",
-      albumCover: smiths, 
+      albumCover: smiths,
       left: "40%",
       top: "13%",
     },
     {
       title: "Sunsetz",
       artist: "Cigarettes After Sex",
-      albumCover: sunsetz, 
+      albumCover: sunsetz,
       left: "10%",
       top: "25%",
     },
-     {
+    {
       title: "Scarface",
       artist: "Booba",
-      albumCover: booba, 
-      left: '35%',
-      top: '38%',
+      albumCover: booba,
+      left: "35%",
+      top: "38%",
     },
-     {
+    {
       title: "Eyes Without A Face",
       artist: "Billy Idol",
-      albumCover: billy, 
-      left: '15%',
-      top: '50%',
+      albumCover: billy,
+      left: "15%",
+      top: "50%",
     },
     {
       title: "Loving Machine",
       artist: "Tv Girl",
-      albumCover: tvGirl, 
-      left: '30%',
-      top: '63%',
+      albumCover: tvGirl,
+      left: "30%",
+      top: "63%",
     },
     {
       title: "I thought i saw your face today",
       artist: "She and Him",
-      albumCover: sheAndHim, 
-      left: '5%',
-      top: '74%',
+      albumCover: sheAndHim,
+      left: "5%",
+      top: "74%",
     },
     {
       title: "MBK Rocket",
       artist: "Lorenzo",
-      albumCover: lorenzo, 
-      left: '25%',
-      top: '86%',
+      albumCover: lorenzo,
+      left: "25%",
+      top: "86%",
     },
   ];
   const containerRef = useRef(null);
 
   return (
     <div className="min-h-screen bg-black/20 flex flex-col items-center justify-center">
-    <div className="w-[90%] max-w-[400px]">
-      <h1 className="text-2xl sm:text-2xl font-bold -mb-4 mt-4 drop-shadow-lg text-white text-center">
-        Songs that remind me of you
-      </h1>
+      <div className="w-[90%] max-w-[400px]">
+        <h1 className="text-2xl sm:text-2xl font-bold -mb-4 mt-4 drop-shadow-lg text-white text-center">
+          Songs that remind me of you
+        </h1>
 
-      <div ref={containerRef}  className=" relative w-full h-[40rem] rounded-lg overflow-hidden mt-8 mb-12">
-        {songs.map((song, index) => (
-          <motion.div
-            key={index}
-            className="absolute" // Use absolute positioning
-            style={{
-              left: song.left,
-              top: song.top,
-            }}
-            drag
-            dragConstraints={containerRef} // Adjust to container size
-          >
-            <div className="bg-white/10 backdrop-blur-lg rounded-xl p-4 flex items-center gap-4 w-56 h-[4rem]">
-              <div className="w-12 h-12 flex-shrink-0">
-                <img
-                  src={song.albumCover}
-                  alt="Album cover"
-                  className="w-full h-full rounded-md object-cover"
-                />
-              </div>
-              <div className="flex-1 min-w-0">
-                <h2 className="text-white font-medium text-sm truncate">
-                  {song.title}
-                </h2>
-                <p className="text-white/70 text-xs truncate">{song.artist}</p>
-              </div>
-            </div>
-          </motion.div>
-        ))}
-      </div>
-
-      {/* Navigation Button */}
-      <div className="flex justify-center w-full mt-4 mb-4">
-        <button
-          className="px-4 py-2 flex justify-center items-center bg-white/20 gap-2 hover:bg-white/30 backdrop-blur-sm text-white text-sm border border-white/50 rounded-lg"
-          onClick={() => navigate("/recap")}
+        <div
+          ref={containerRef}
+          className=" relative w-full h-[40rem] rounded-lg overflow-hidden mt-8 mb-12"
         >
-          <ArrowLeft /> Previous page
-        </button>
+          {songs.map((song, index) => (
+            <motion.div
+              key={index}
+              className="absolute" // Use absolute positioning
+              style={{
+                left: song.left,
+                top: song.top,
+              }}
+              drag
+              dragConstraints={containerRef} // Adjust to container size
+            >
+              <div className="bg-white/10 backdrop-blur-lg rounded-xl p-4 flex items-center gap-4 w-56 h-[4rem]">
+                <div className="w-12 h-12 flex-shrink-0">
+                  <img
+                    src={song.albumCover}
+                    alt="Album cover"
+                    className="w-full h-full rounded-md object-cover"
+                  />
+                </div>
+                <div className="flex-1 min-w-0">
+                  <h2 className="text-white font-medium text-sm truncate">
+                    {song.title}
+                  </h2>
+                  <p className="text-white/70 text-xs truncate">
+                    {song.artist}
+                  </p>
+                </div>
+              </div>
+            </motion.div>
+          ))}
+        </div>
+
+        {/* Navigation Button */}
+        <div className="flex justify-center w-full mt-4 mb-4">
+          <button
+            className="px-4 py-2 flex justify-center items-center bg-white/20 gap-2 hover:bg-white/30 backdrop-blur-sm text-white text-sm border border-white/50 rounded-lg"
+            onClick={() => navigate("/recap")}
+          >
+            <ArrowLeft /> Previous page
+          </button>
+        </div>
       </div>
     </div>
-  </div>
   );
 }
 
--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,63 +1,102 @@
-import React, { useRef, useEffect } from "react";
-import { basket, iceCream, biscuit, mochi, kitkat, macaron } from "../assets";
+import { useRef, useEffect } from "react";
+import {
+  basket,
+  iceCream,
+  biscuit,
+  mochi,
+  kitkat,
+  macaron,
+  lorenzo,
+  kidCudi,
+  sunsetz,
+} from "../assets";
+import { motion } from "motion/react";
+
+// plus tu veux descendre plsu tu augemntes le top
+// plus tu. veux mettre a droite plus tu augmentes le left et dcp tu le. baisses pr aller a gauche
 
 function Playground() {
-  const imgRef = useRef(null);
+  const containerRef = useRef(null);
 
-  useEffect(() => {
-    const img = imgRef.current;
-
-    let isDragging = false;
-    let offsetX, offsetY;
-
-    const handleMouseDown = (e) => {
-      isDragging = true;
-      offsetX = e.clientX - img.offsetLeft;
-      offsetY = e.clientY - img.offsetTop;
-      img.style.cursor = "grabbing";
-    };
-
-    const handleMouseMove = (e) => {
-      if (!isDragging) return;
-
-      img.style.left = e.clientX - offsetX + "px";
-      img.style.top = e.clientY - offsetY + "px";
-    };
-
-    const handleMouseUp = () => {
-      isDragging = false;
-      img.style.cursor = "grab";
-    };
-
-    img.addEventListener("mousedown", handleMouseDown);
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp);
-
-    // cleanup important !!
-    return () => {
-      img.removeEventListener("mousedown", handleMouseDown);
-      document.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mouseup", handleMouseUp);
-    };
-  }, []);
+  const images = [
+    {
+      img: iceCream,
+      left: "5%",
+      top: "2%",
+    },
+    {
+      img: biscuit,
+      left: "20%",
+      top: "73%",
+    },
+    {
+      img: mochi,
+      left: "10%",
+      top: "25%",
+    },
+    {
+      img: kitkat,
+      left: "65%",
+      top: "38%",
+    },
+    {
+      img: macaron,
+      left: "15%",
+      top: "60%",
+    },
+    {
+      img: lorenzo,
+      left: "70%",
+      top: "63%",
+    },
+    {
+      img: kidCudi,
+      left: "65%",
+      top: "74%",
+    },
+    {
+      img: sunsetz,
+      left: "85%",
+      top: "67%",
+    },
+  ];
 
   return (
-    <div className="min-h-screen bg-green-200 relative">
+    <div className="max-h-screen bg-green-200 relative">
       <h1 className="text-4xl font-bold mb-8 text-white text-center">
         Playground time !
       </h1>
 
-      <img src={basket} alt="" />
-      <img
-        src={iceCream}
-        ref={imgRef}
-        className="absolute cursor-grab"
-        alt=""
-      />
-      <img src={biscuit} alt="" />
-      <img src={mochi} alt="" />
-      <img src={kitkat} alt="" />
-      <img src={macaron} alt="" />
+      <div
+        ref={containerRef}
+        className=" relative min-h-screen rounded-lg overflow-hidden mt-8 mb-12"
+      >
+        <img
+          src={basket}
+          className="absolute top-[15%] left-[35%] w-[600px] h-[600px]"
+        />
+
+        {images.map((img, index) => (
+          <motion.div
+            key={index}
+            className="absolute" // Use absolute positioning
+            style={{
+              left: img.left,
+              top: img.top,
+            }}
+            drag
+            dragConstraints={containerRef} // Adjust to container size
+          >
+            <img
+              src={img.img}
+              alt=""
+              className="rounded-md object-cover h-[100px] w-auto"
+              draggable="false" // on a mis ca pr dire quon veut pas deplacer limage mais la div qui est au dessus qui contient limage
+            />
+          </motion.div>
+        ))}
+      </div>
+      <div />
     </div>
   );
 }
